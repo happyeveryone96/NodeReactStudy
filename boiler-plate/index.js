a0874587a6777bc27fe6d595f316f61216148734
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-23 17:25:17 +0900

    feat: MongoDB 연결

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+require('dotenv').config({path : '.env'});
+
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://${process.env.ID}:${process.env.PW}@boilerplate.ou3bg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
