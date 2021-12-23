83762cc64b85c95527adce4d3dd812e0af2b083b
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-23 18:29:39 +0900

    feat: 회원가입 기능 추가

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
 require('dotenv').config({path : '.env'});
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(`mongodb+srv://${process.env.ID}:${process.env.PW}@boilerplate.ou3bg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(() => console.log('MongoDB Connected...'))
@@ -10,6 +18,19 @@ mongoose.connect(`mongodb+srv://${process.env.ID}:${process.env.PW}@boilerplate.
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
