d6fcae01a5b89cbfa26cf6c0735aa218bf1aef9a
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-23 19:55:47 +0900

    feat: 로그인 기능 추가

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -33,22 +33,34 @@ const userSchema = mongoose.Schema({
   }
 })
 
-userSchema.pre('save', function( next ) {
+userSchema.pre('save', function (next) {
   var user = this;
   if (user.isModified('password')) {
     // 비밀번호를 암호화시킨다.
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-      if (err) return next (err)
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err)
 
-      bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) return next (err)
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err)
         user.password = hash
         next()
       });
     });
+  } else {
+    next();
   }
 })
 
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 1234567   암호화된 비밀번호 $2b$10$rSK73yfwMebqyDAZYK3EU
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err),
+      cb(null, isMatch)
+  })
+}
+
 const User = mongoose.model('User', userSchema)
 
-module.exports = { User }
+module.exports = {
+  User
+}
