3f09dde8a32b8ea4aef951a82599d24e08f00866
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-12-24 22:27:46 +0900

    feat: 로그아웃 버튼 추가

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,25 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get('/api/hello')
     .then(res => console.log(res.data));
   }, [])
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+    .then(response => {
+      if (response.data.success) {
+        navigate('/login');
+      } else {
+        alert('로그아웃 하는데 실패했습니다.')
+      }
+    })
+  }
 
   return (
     <div 
@@ -15,7 +29,10 @@ function LandingPage() {
         alignItems: 'center',
         width: '100%',
         height: '100vh'}}>
-      시작 페이지
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   )
 }
