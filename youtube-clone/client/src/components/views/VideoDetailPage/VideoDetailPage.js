fb3d872d2ced7749c8b5438184ef3ff726de2cde
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-05 18:44:55 +0900

    feat: 댓글 컴포넌트 및 댓글 모델 추가

--- youtube-clone/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ youtube-clone/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
+import Comment from './Sections/Comment';
 
 function VideoDetailPage(props) {
   const videoId = props.match.params.videoId;
@@ -37,7 +38,7 @@ function VideoDetailPage(props) {
                 description={videoDetail.description}
                 />
             </List.Item>
-  
+            <Comment/>
           </div>
         </Col>
         <Col lg={6} xs={24}>
